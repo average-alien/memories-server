1cb298f70b2c8b51a60998bc478d028c404d2a44
average-alien <allentang113@gmail.com>
2022-10-07 12:32:19 -0700

    adds get and post routes

---
+++ controllers/api-v1/memories.js
@@ -0,0 +1,35 @@
+const router = require('express').Router()
+const db = require('../../models')
+const jwt = require('jsonwebtoken')
+const authLockedRoute = require('./authLockedRoute')
+
+router.get('/', authLockedRoute, async (req, res) => {
+    try {
+        const currentUser = res.locals.user.populate({
+            path: 'memories',
+            populate: {
+                path: 'user'
+            }
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ msg: 'server error'  })
+    }
+})
+
+router.post('/', authLockedRoute, async (req, res) => {
+    try {
+        const newMemory = db.Memory.create(req.body)
+        res.locals.user.memories.push(newMemory)
+        newMemory.user = res.locals.user
+        await newMemory.save()
+        await res.locals.user.save()
+        res.status(204).json(newMemory)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ msg: 'server error'  })
+    }
+})
+
+module.exports = router
